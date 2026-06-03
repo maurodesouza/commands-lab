2a9e086d79d3787421e4da6a88b987b8bcd26242
Mauro de Souza <maurodesouza.me@gmail.com>
2026-06-03 00:16:59 -0300

    ✨ add PathValue, PayloadFromAction, and ReturnFromAction type helpers for enhanced type inference

--- src/types/helpers.ts
+++ src/types/helpers.ts
@@ -4,11 +4,30 @@ export type DeepKeyPaths<T> = T extends object
 		}[keyof T & string]
 	: never;
 
+export type DeepKeys<T> = T extends object
+	? {
+			[K in keyof T]: K | DeepKeys<T[K]>;
+		}[keyof T]
+	: never;
 
-export type DeepKeys<T> =
-  T extends object
-    ? {
-        [K in keyof T]:
-          K | DeepKeys<T[K]>
-      }[keyof T]
-    : never;
+export type PathValue<
+	T,
+	P extends string,
+> = P extends `${infer K}.${infer Rest}`
+	? K extends keyof T
+		? PathValue<T[K], Rest>
+		: never
+	: P extends keyof T
+		? T[P]
+		: never;
+
+export type PayloadFromAction<T> = T extends (
+	payload: infer P,
+	...args: any[]
+) => any
+	? P
+	: never;
+
+export type ReturnFromAction<T> = T extends (...args: any[]) => infer R
+	? Awaited<R>
+	: never;
